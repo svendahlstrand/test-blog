6382f1c90b13c3dfa811aed127ec559e9cda6b5a
Sven Dahlstrand <sven@dahlstrand.net>
2023-05-09 17:45:41 +0200

    Update micropost permalink page test to use microformats helper.

--- tests/microformats_test.js
+++ tests/microformats_test.js
@@ -1,9 +1,10 @@
 import { assertExists, assertEquals, assertStringIncludes } from "https://deno.land/std@0.181.0/testing/asserts.ts";
-import { loadPublicHtmlFile } from "./helpers/load-html-file.js"
+import { loadPublicHtmlFile, mfFromPublicHtmlFile } from "./helpers/load-html-file.js"
 import { closest } from "./helpers/closest.js"
 
 const homepage = loadPublicHtmlFile("./index.html");
 const feed = homepage.querySelector('.h-feed');
+const first = _ => true;
 
 Deno.test("Feed microformats are present on homepage", () => {
   assertExists(feed, "No feed microformats found.");
@@ -49,22 +50,24 @@ Deno.test("Micropost entry have valid microformats on homepage", () => {
 });
 
 Deno.test("Micropost have valid microformats on permalink page", () => {
-  const micropost = loadPublicHtmlFile("./2023/04/20/happy-earth-day.html");
-  const micropostEntry = micropost.querySelector('.h-entry');
-  const micropostContent = micropostEntry.querySelector('.e-content');
-  const micropostPublished = micropostEntry.querySelector('.dt-published');
-  const micropostURL = micropostEntry.querySelector('.u-url');
+  const micropost = mfFromPublicHtmlFile("/2023/04/20/happy-earth-day.html");
+  const micropostEntry = micropost.items.find(item => item.type.includes('h-entry'))
 
-  // TODO: Start using a proper mf2 parser instead. The following is a bit to
-  //       naive and won't work with themes that list authors, for example.
-  //assertEquals(micropostEntry.querySelector('.p-name'), null, 'Micropost should not have p-name microformat.');
+  assertExists(micropostEntry, "No h-entry found on page.");
+
+  const micropostName = micropostEntry.properties.name?.find(first);
+  const micropostContent = micropostEntry.properties.content?.find(first).value;
+  const micropostPublished =  micropostEntry.properties.published?.find(first);
+  const micropostURL = micropostEntry.properties.url?.find(first);
+
+  assertEquals(micropostName, undefined, 'Micropost should not have p-name microformat.');
 
   assertExists(micropostContent, 'Micropost should have e-content microformat.');
-  assertStringIncludes(micropostContent.textContent, 'Happy Earth Day!', 'e-content should contain post body.');
+  assertStringIncludes(micropostContent, 'Happy Earth Day!', 'e-content should contain post body.');
 
   assertExists(micropostPublished, 'Micropost should have dt-published microformat.');
-  assertStringIncludes(micropostPublished.getAttribute('datetime'), '2023-04-20', 'Publish date should be correct.');
+  assertStringIncludes(micropostPublished, '2023-04-20', 'Publish date should be correct.');
 
   assertExists(micropostURL, 'Micropost should have u-url microformat.');
-  assertStringIncludes(micropostURL.getAttribute('href'), '/2023/04/20/happy-earth-day.html', 'URL should be correct.');
+  assertStringIncludes(micropostURL, '/2023/04/20/happy-earth-day.html', 'URL should be correct.');
 });
