34e354589f34005ce623cbe08712fb63ea0e8c57
Sven Dahlstrand <sven@dahlstrand.net>
2023-05-09 13:57:37 +0200

    Add tests for p-name and e-content in feed for both posts and microposts.

--- tests/microformats_test.js
+++ tests/microformats_test.js
@@ -1,11 +1,33 @@
-import { assertExists } from "https://deno.land/std@0.181.0/testing/asserts.ts";
+import { assertExists, assertEquals, assertStringIncludes } from "https://deno.land/std@0.181.0/testing/asserts.ts";
 import { loadPublicHtmlFile } from "./helpers/load-html-file.js"
+import { closest } from "./helpers/closest.js"
+
+const homepage = loadPublicHtmlFile("./index.html");
+const feed = homepage.querySelector('.h-feed');
 
 Deno.test("Feed microformats are present on homepage", () => {
-  const document = loadPublicHtmlFile("./index.html");
-  const feed = document.querySelector('.h-feed');
-  const firstEntry = feed?.querySelector('.h-entry');
-
   assertExists(feed, "No feed microformats found.");
-  assertExists(firstEntry, "No feed entries found.");
+  assertExists(feed.querySelector('.h-entry'), "No h-entry found in h-feed.");
+});
+
+Deno.test("Post entry have valid microformats on homepage", () => {
+  const postEntry = closest('.h-entry', feed?.querySelector('a[href$="/2023/03/27/the-joy-of.html"]'));
+  const postName = postEntry.querySelector('.p-name');
+  const postContent = postEntry.querySelector('.e-content');
+
+  assertExists(postName, 'Post should have p-name microformat.');
+  assertEquals(postName.textContent, 'The Joy of Having a Personal Blog', 'p-name should be the title.');
+
+  assertExists(postContent, 'Post should have e-content microformat.');
+  assertStringIncludes(postContent.textContent, 'Blogging has become increasingly popular', 'e-content should contain post body.');
+});
+
+Deno.test("Micropost entry have valid microformats on homepage", () => {
+  const micropostEntry = closest('.h-entry', feed?.querySelector('a[href$="/2023/04/20/happy-earth-day.html"]'));
+  const micropostContent = micropostEntry.querySelector('.e-content');
+
+  assertEquals(micropostEntry.querySelector('.p-name'), null, 'Micropost should not have p-name microformat.');
+
+  assertExists(micropostContent, 'Micropost should have e-content microformat.');
+  assertStringIncludes(micropostContent.textContent, 'Happy Earth Day!', 'e-content should contain post body.');
 });
