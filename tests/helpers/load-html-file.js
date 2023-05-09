5e254f62f9e9b6bfebd73b9e308f366260e064ac
Sven Dahlstrand <sven@dahlstrand.net>
2023-05-09 17:44:56 +0200

    Add microformat test helper.

--- tests/helpers/load-html-file.js
+++ tests/helpers/load-html-file.js
@@ -1,4 +1,5 @@
 import { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
+import { mf2 } from 'https://cdn.jsdelivr.net/npm/microformats-parser@1.4.1/+esm'
 import { dirname, normalize, fromFileUrl } from "https://deno.land/std@0.184.0/path/mod.ts";
 
 const moduleRelativePath = (path) => {
@@ -8,4 +9,11 @@ const moduleRelativePath = (path) => {
 export const loadPublicHtmlFile = (path) => {
   const html = Deno.readTextFileSync(moduleRelativePath(`/../../public/${path}`));
   return new DOMParser().parseFromString(html, "text/html");
+};
+
+export const mfFromPublicHtmlFile = (path) => {
+  const html = Deno.readTextFileSync(moduleRelativePath(`/../../public/${path}`));
+  return mf2(html, {
+    baseUrl: "https://janedoe.micro.blog",
+  });
 };
