cd0bebed2351ef5934deeb54f032d97db62f9e9f
Sven Dahlstrand <sven@dahlstrand.net>
2023-05-11 16:40:49 +0200

    Return null early to get better error messages.

--- tests/helpers/closest.js
+++ tests/helpers/closest.js
@@ -1,5 +1,7 @@
 // deno-dom lack support for closest so use this for now.
 export const closest = function (s, el) {
+    if (!el) return null;
+
     do {
         if (el.matches(s)) return el;
         el = el.parentElement || el.parentNode;
